4edb2ee0fd7d9014a81d820eef0bf40c2c363322
Lucas <lucas14guesser@gmail.com>
2023-10-05 22:23:51 -0300

    teste1

--- src/components/adotar/ComponentesAdotar.jsx
+++ src/components/adotar/ComponentesAdotar.jsx
@@ -21,7 +21,7 @@ const ComponentesAdotar = () => {
         try {
             setEnvioEmAndamento(true);
 
-            const rota = 'https://adote-seu-cao.vercel.app:443/formulario';
+            const rota = 'https://adote-seu-cao-lucas14guesser.vercel.app/formulario';
             const resp = await axios.post(rota, {
                 nome: dadosFormulario.nome,
                 sobrenome: dadosFormulario.sobrenome,
